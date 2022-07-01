cc4e52df40110a1967d49531d9bfb79a5df356cb
Lucas Alves <39414546+lucasfdelis@users.noreply.github.com>
2022-07-01 17:53:56 -0300

    Contador binário

--- src/main.c
+++ src/main.c
@@ -24,20 +24,23 @@ int main(void)
 	RCC->AHB1ENR |= 1;				//habilita o clock do GPIOA
 
 	GPIOA->MODER |= (1 << 12);		//seleciona modo de saída digital no pino
-
+	GPIOA->MODER |= (1 << 14);
 	while(1)
 	{
+		for (int i=0; i<=3; i++)
+		{
+			GPIOA->ODR = ~i << 6;
+			atraso(6000000);
+		}
 
-		int valor = 1000000;
 
-				while(valor != 0){
-					GPIOA->ODR &= ~(1 << 6);	//faz o estado do pino como LOW
-					atraso(100000);
-					GPIOA->ODR |= (1 << 6);		//faz o estado do pino como HIGH
-					atraso(valor);
-					valor = valor - 10000;
 
-				}
+
+
+
+
+
+
 //		GPIOA->ODR &= ~(1 << 6);	//faz o estado do pino como LOW
 //		atraso(300000);
 //		GPIOA->ODR |= (1 << 6);		//faz o estado do pino como HIGH
